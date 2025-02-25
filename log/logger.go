b401405a2f2b63cb5f719e4c25e12dcca8e23555
khdream <136836718+khdream@users.noreply.github.com>
2025-02-25 22:44:54 +0900

    Update logger.go

--- log/logger.go
+++ log/logger.go
@@ -1,12 +1,23 @@
-package log
+package mylog // Consider renaming the package to avoid conflict with the standard library  
 
-import log "github.com/cihub/seelog"
+import (  
+    "os"  
+    "github.com/cihub/seelog"  
+)  
 
-//return main file path
-func InitLogger(cfgfile string) {
-	logger, err := log.LoggerFromConfigAsFile(cfgfile)
-	if err != nil {
-		panic(err)
-	}
-	log.ReplaceLogger(logger)
+// InitLogger initializes the logger with the configuration file given.  
+// It returns an error if the configuration file cannot be loaded.  
+func InitLogger(cfgfile string) error {  
+    // Check if the configuration file exists  
+    if _, err := os.Stat(cfgfile); os.IsNotExist(err) {  
+        return err // Return an error if the file does not exist  
+    }  
+
+    logger, err := seelog.LoggerFromConfigAsFile(cfgfile)  
+    if err != nil {  
+        return err // Return the error instead of panicking  
+    }  
+
+    seelog.ReplaceLogger(logger)  
+    return nil // Return nil if initialization is successful  
 }
